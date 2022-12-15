97de447cf17356b9611126d918d8ccdb7467b863
hem193 <hemanthcastel@gmail.com>
2022-12-15 19:48:37 +0530

    Practise

---
+++ material-ui-prac/src/Test.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { Button, TextField } from "@mui/material";
+const Test = () => {
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    password: " ",
+  });
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          name="name"
+          value={inputs.name}
+          onChange={handleChange}
+          sx={{ margin: 3 }}
+          placeholder="Name"
+          variant="outlined"
+        />
+        <TextField
+          name="email"
+          value={inputs.email}
+          onChange={handleChange}
+          type={"email"}
+          sx={{ margin: 3 }}
+          placeholder="Email"
+          variant="filled"
+        />
+        <TextField
+          name="password"
+          value={inputs.password}
+          onChange={handleChange}
+          type={"password"}
+          sx={{ margin: 3 }}
+          placeholder="Password"
+          variant="standard"
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </div>
+  );
+};
+
+export default Test;
